5de6c5849915dedb776212dadba53b251853eefd
alysonmcechinel <alysonmcechinell@gmail.com>
2023-05-17 22:24:21 -0300

    exe 1 - implementar um novo imposto com pattern strategy

--- CursoDesignPatters/Program.cs
+++ CursoDesignPatters/Program.cs
@@ -1,21 +1,40 @@
 ﻿namespace CursoDesignPatters
 {
-    class Program
+    /*class Program
     {
         static void Main(string[] args)
         {
 
             Imposto iss = new ISS();
 			Imposto icms = new ICMS();
+			//Imposto iccc = new ICCC();
+			//calculador.RealizarCalculo(orcamento, iccc);
 
-            Orcamento orcamento = new Orcamento(500.0);
+			Console.WriteLine("escreva o valor total do orçamento");
+			double total = Convert.ToDouble(Console.ReadLine());
 
-            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+			Orcamento orcamento = new Orcamento(total);
+			CalculadorDeImpostos calculador = new CalculadorDeImpostos();
 
-            calculador.RealizarCalculo(orcamento, icms);
+			calculador.RealizarCalculo(orcamento, icms);
 			calculador.RealizarCalculo(orcamento, iss);
 
-            Console.ReadKey();
-		}        
-    }
+			Console.ReadKey();
+		}
+	}*/
+
+	
+
+	class Exec1
+	{
+		static void Main(String[] args)
+		{
+			Orcamento reforma = new Orcamento(500);
+			//Orcamento reforma = new Orcamento(1500);
+			//Orcamento reforma = new Orcamento(4000);
+
+			Imposto novoImposto = new ICCC();
+			Console.WriteLine(novoImposto.Calcula(reforma));
+		}
+	}
 }
